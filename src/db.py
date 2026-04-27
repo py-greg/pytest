ebc3afd6494969cfae41ee751e02b4222cb0608c
py-greg <grihadavydov1972@gmail.com>
2026-04-27 15:59:05 +0300

    five. hundred. syntax errors.

--- src/db.py
+++ src/db.py
@@ -7,48 +7,47 @@ conn = pymysql.connect(
     port=3306,
     user="greg",
     password="Wasthatthebiteof87",
-    database="greg_db"
+    database="greg_db",
+    cursorclass=pymysql.cursors.DictCursor
 )
 
 #all functions are self-explanatory
 def get_users() -> List[Profile]:
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM users")
-    return [Profile(**row) for row in cursor]
+    return [Profile(**row) for row in cursor.fetchall()]
 
 def my_profile(user_id: int) -> Profile:
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
-    return Profile(**cursor.fetchone())
+    cursor.execute("SELECT * FROM users WHERE id = %s", (user_id,))
+    return Profile(**cursor.fetchall()[0])
 
 def get_chats(user_id: int) -> List[Chat]:
     cursor = conn.cursor()
-    cursor.execute("SELECT chats.id, chats.name FROM users INNER JOIN u2c ON u2c.uid = (?) INNER JOIN chats ON chats.id = u2c.cid",
-        [user_id]
+    cursor.execute("SELECT chats.id, chats.name FROM users INNER JOIN u2c ON u2c.uid = %s INNER JOIN chats ON chats.id = u2c.cid",
+        (user_id,)
     )
-    chats = cursor.fetchall()
-    return [Chat(**chat) for chat in chats]
+    return [Chat(**chat) for chat in cursor.fetchall()]
 
 def register_user(user: Profile) -> bool:
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO users (name, age, email, phone, country) VALUES (?, ?, ?, ?, ?)",
-        (user.name, user.age, user.email, user.phone, user.country)
+    cursor.execute("INSERT INTO users (name, age, email, phone, country) VALUES (%s, %s, %s, %s, %s)",
+        (user.name, user.age, user.email, user.phone, user.country,)
     )
     conn.commit()
     return True
 
 def create_chat(chat: Chat) -> bool:
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO chats (name) VALUES (?)",
-        (chat.name)
+    cursor.execute("INSERT INTO chats (name) VALUES (%s)",
+        (chat.name,)
     )
     conn.commit()
     return True
 
 def get_messages(chat_id: int) -> List[Message]:
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM messages WHERE chat_id = ?",
+    cursor.execute("SELECT * FROM messages WHERE chat_id = %s",
         (chat_id,)
     )
-    messages = cursor.fetchall()
-    return [Message(**message) for message in messages]
+    return [Message(**message) for message in cursor.fetchall()]
